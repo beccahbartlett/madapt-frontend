b3ad748cf35bfe9146013800b62ed0f91f637cbc
srinivaasang1 <srinivaasan.g1@gmail.com>
2017-06-17 16:00:58 +1000

    Adding Clinic page
    Clinic page - CSS, html. images

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './home/Home'
+import Clinic from './clinic/Clinic'
 import About from './about/About'
 import Nav from './nav/Nav'
 import Footer from './footer/Footer'
@@ -18,6 +19,7 @@ class App extends Component {
           <Nav />
           <Switch>
             <Route exact path='/' component={Home} />
+            <Route exact path='/clinic' component={Clinic} />
             <Route exact path='/about' component={About} />
             <Route render={function() {
               return <p>Not Found</p>
