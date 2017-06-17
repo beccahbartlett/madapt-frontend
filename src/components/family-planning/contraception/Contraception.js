199a2075a81598c1ad3a8062c74d6403831aef04
srinivaasang1 <srinivaasan.g1@gmail.com>
2017-06-18 09:27:36 +1000

    sub-category : family-planning renaming, getting rid of accrodian

--- src/components/family-planning/contraception/Contraception.js
+++ src/components/family-planning/contraception/Contraception.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'flexbox-react';
-import './SubCategory.css'
+import './Contraception.css'
 
 var Accordion = require('react-bootstrap').Accordion;
 var Panel = require('react-bootstrap').Panel;
 
 
-class SubCategory extends Component {
+class Contraception extends Component {
   render() {
     return (
       <div>
@@ -24,11 +24,11 @@ class SubCategory extends Component {
             Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
           </Panel>
           <Panel header="Collapsible Group Item #3" eventKey="3">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. 
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
           </Panel>
         </Accordion>
       </div>
     )
   }
 }
-export default SubCategory
+export default Contraception
