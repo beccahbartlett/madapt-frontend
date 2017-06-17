e9aada01e56aba2b0b26db47485bb473c625b735
James Liu <james_liu@fastmail.com>
2017-06-17 16:20:22 +1000

    added nav menu icons

--- src/components/navmenu/NavMenu.js
+++ src/components/navmenu/NavMenu.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Navbar, NavItem, Nav } from 'react-bootstrap'
 import './NavMenu.css'
 import iconLocation from '../../images/icons/location.png'
+import iconPopular from '../../images/icons/popular-topics.png'
+import iconRelationships from '../../images/icons/relationships.png'
+import iconFamily from '../../images/icons/family-planning.png'
+import iconHealthcare from '../../images/icons/health-care-system.png'
+import iconReproductive from '../../images/icons/reproductive.png'
 
 class NavMenu extends Component {
   render() {
@@ -12,11 +17,23 @@ class NavMenu extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} href="#">Link
+            <NavItem eventKey={1} href="#">Clinics
               <img src={iconLocation} />
             </NavItem>
-            <NavItem eventKey={2} href="#">Link
-              <img src={iconLocation} />
+            <NavItem eventKey={2} href="#">Popular Topics
+              <img src={iconPopular} />
+            </NavItem>
+            <NavItem eventKey={3} href="#">Relationships
+              <img src={iconRelationships} />
+            </NavItem>
+            <NavItem eventKey={4} href="#">Family Planning
+              <img src={iconFamily} />
+            </NavItem>
+            <NavItem eventKey={5} href="#">Health Care System
+              <img src={iconHealthcare} />
+            </NavItem>
+            <NavItem eventKey={6} href="#">Reproductive Health
+              <img src={iconReproductive} />
             </NavItem>
           </Nav>
         </Navbar.Collapse>
