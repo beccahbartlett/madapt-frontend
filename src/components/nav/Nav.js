177d1125841ae4d59544c516c36f7bf3c3391767
James Liu <james_liu@fastmail.com>
2017-06-17 16:52:42 +1000

    refactor Nav and NavMenu components

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
 import './Nav.css'
 import NavMenu from '../navmenu/NavMenu'
+// import icons for Webpack
+import iconLocation from '../../images/icons/location.png'
+import iconPopular from '../../images/icons/popular-topics.png'
+import iconRelationships from '../../images/icons/relationships.png'
+import iconFamily from '../../images/icons/family-planning.png'
+import iconHealthcare from '../../images/icons/health-care-system.png'
+import iconReproductive from '../../images/icons/reproductive.png'
 
 class Nav extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      menuItems: [
+        {
+          icon: iconLocation,
+          text: 'Clinics'
+        },
+        {
+          icon: iconPopular,
+          text: 'Popular Topics'
+        },
+        {
+          icon: iconRelationships,
+          text: 'Relationships'
+        },
+        {
+          icon: iconFamily,
+          text: 'Family Planning'
+        },
+        {
+          icon: iconHealthcare,
+          text: 'Health Care System'
+        },
+        {
+          icon: iconReproductive,
+          text: 'Reproductive Health'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="nav">
@@ -10,7 +49,7 @@ class Nav extends Component {
           <h1 className="nav-header-title">mAdapt</h1>
         </div>
         <div className="nav-search"></div>
-        <NavMenu />
+        <NavMenu menuButtons={this.state.menuItems}/>
       </div>
     )
   }
--- src/components/navmenu/NavMenu.js
+++ src/components/navmenu/NavMenu.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavItem, Nav } from 'react-bootstrap'
 import './NavMenu.css'
-import iconLocation from '../../images/icons/location.png'
-import iconPopular from '../../images/icons/popular-topics.png'
-import iconRelationships from '../../images/icons/relationships.png'
-import iconFamily from '../../images/icons/family-planning.png'
-import iconHealthcare from '../../images/icons/health-care-system.png'
-import iconReproductive from '../../images/icons/reproductive.png'
 
 class NavMenu extends Component {
   render() {
@@ -17,24 +11,13 @@ class NavMenu extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} href="#">Clinics
-              <img className="navbar-menu-icon" src={iconLocation} />
-            </NavItem>
-            <NavItem eventKey={2} href="#">Popular Topics
-              <img className="navbar-menu-icon" src={iconPopular} />
-            </NavItem>
-            <NavItem eventKey={3} href="#">Relationships
-              <img className="navbar-menu-icon" src={iconRelationships} />
-            </NavItem>
-            <NavItem eventKey={4} href="#">Family Planning
-              <img className="navbar-menu-icon" src={iconFamily} />
-            </NavItem>
-            <NavItem eventKey={5} href="#">Health Care System
-              <img className="navbar-menu-icon" src={iconHealthcare} />
-            </NavItem>
-            <NavItem eventKey={6} href="#">Reproductive Health
-              <img className="navbar-menu-icon" src={iconReproductive} />
-            </NavItem>
+            {this.props.menuButtons.map(function(item, index) {
+              return(
+                <NavItem eventKey={index} href="#">{item.text}
+                  <img className="navbar-menu-icon" src={item.icon} />
+                </NavItem>
+              )
+            })}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
