d6d8569660666e65df0c8e88e5547a980f6f09d0
James Liu <james_liu@fastmail.com>
2017-06-18 11:17:07 +1000

    refactored SexualHealth component

--- src/components/sexual-health/SexualHealth.js
+++ src/components/sexual-health/SexualHealth.js
@@ -1,57 +1,38 @@
 import React, { Component } from 'react';
+import CardGrid from '../cardgrid/CardGrid';
+import Card from '../card/Card';
 import { Grid, Row, Col } from 'flexbox-react';
 import './SexualHealth.css'
 
 class SexualHealth extends Component {
   render() {
     return (
-      <div>
-      SexualHealth
-        <main className="cards">
-          <article className="card">
-            <img src="https://image.flaticon.com/icons/png/512/33/33777.png"/>
-            <div className="text">
-              <p>STI's</p>
-            </div>
-          </article>
-          <article className="card">
-            <img src="https://image.flaticon.com/icons/png/512/33/33777.png"/>
-            <div className="text">
-              <p>HIV & Aids</p>
-            </div>
-          </article>
-          <article className="card">
-            <img src="https://image.flaticon.com/icons/png/512/33/33777.png"/>
-            <div className="text">
-              <p>Sexuality</p>
-            </div>
-          </article>
-          <article className="card">
-            <img src="https://image.flaticon.com/icons/png/512/33/33777.png"/>
-            <div className="text">
-              <p>Content</p>
-            </div>
-          </article>
-          <article className="card">
-            <img src="https://image.flaticon.com/icons/png/512/33/33777.png"/>
-            <div className="text">
-              <p>Disability</p>
-            </div>
-          </article>
-          <article className="card">
-            <img src="https://image.flaticon.com/icons/png/512/33/33777.png"/>
-            <div className="text">
-              <p>Safe sex</p>
-            </div>
-          </article>
-          <article className="card">
-            <img src="https://image.flaticon.com/icons/png/512/33/33777.png"/>
-            <div className="text">
-              <p>Services</p>
-            </div>
-          </article>
-        </main>
-      </div>
+      <CardGrid>
+        <Card
+          imgSrc="https://image.flaticon.com/icons/png/512/33/33777.png"
+          titleText="STI's"
+          linkUrl="#" />
+        <Card
+          imgSrc="https://image.flaticon.com/icons/png/512/33/33777.png"
+          titleText="HIV & Aids"
+          linkUrl="#" />
+        <Card
+          imgSrc="https://image.flaticon.com/icons/png/512/33/33777.png"
+          titleText="Sexuality"
+          linkUrl="#" />
+        <Card
+          imgSrc="https://image.flaticon.com/icons/png/512/33/33777.png"
+          titleText="Disability"
+          linkUrl="#" />
+        <Card
+          imgSrc="https://image.flaticon.com/icons/png/512/33/33777.png"
+          titleText="Safe sex"
+          linkUrl="#" />
+        <Card
+          imgSrc="https://image.flaticon.com/icons/png/512/33/33777.png"
+          titleText="Services"
+          linkUrl="#" />
+      </CardGrid>
     )
   }
 }
