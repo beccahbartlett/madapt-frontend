feb58bbcc235500adf947f717542fe461ed4745a
James Liu <james_liu@fastmail.com>
2017-06-18 11:24:08 +1000

    removed unnecessary imports

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -5,16 +5,10 @@ import NavSearch from '../navsearch/NavSearch';
 import { NavLink } from 'react-router-dom';
 // import icons for Webpack
 import iconLocation from '../../images/icons/location.png';
-import iconPopular from '../../images/icons/popular-topics.png';
-import iconRelationships from '../../images/icons/relationships.png';
 import iconChild from '../../images/icons/child.png';
-import iconBrain from '../../images/icons/brain.png';
-import iconFemale from '../../images/icons/female.png';
-import iconMale from '../../images/icons/male.png';
 import iconViolence from '../../images/icons/violence.png';
 import iconFamily from '../../images/icons/family-planning.png';
 import iconLifecycle from '../../images/icons/lifecycle.png';
-import iconHealthcare from '../../images/icons/health-care-system.png';
 import iconReproductive from '../../images/icons/reproductive.png';
 
 class Nav extends Component {
