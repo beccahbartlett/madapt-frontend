8e0ad2428b096f03651af8b3bef8b4360c027244
James Liu <james_liu@fastmail.com>
2017-06-18 11:02:30 +1000

    added Card and CardGrid components

---
+++ src/components/card/Card.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './Card.css'
+
+class Card extends Component {
+  render() {
+    return (
+      <article className="card">
+        <Link to={this.props.linkUrl}>
+          <div>
+            <img src={this.props.imgSrc}/>
+            <div className="text">
+              <p>{this.props.titleText}</p>
+            </div>
+          </div>
+        </Link>
+      </article>
+    )
+  }
+}
+
+export default Card
