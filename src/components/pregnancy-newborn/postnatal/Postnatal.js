8dca557862083cd807586f9dfc0ce49b1245a658
James Liu <james_liu@fastmail.com>
2017-07-08 14:27:02 +1000

    added new content to json, got tabs working (sort of)

--- src/components/pregnancy-newborn/postnatal/Postnatal.js
+++ src/components/pregnancy-newborn/postnatal/Postnatal.js
@@ -8,32 +8,27 @@ import data from '../../../Content/postnatal.json';
 var Accordion = require('react-bootstrap').Accordion;
 var Panel = require('react-bootstrap').Panel;
 
-
-  const contraceptions = [
-    {name: 'Male Condoms', biography: '1Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
-    {name: 'Female Condom', biography: '2Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
-    {name: 'The Pill', biography: '3Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
-  ]
-  function getTabs() {
-    return contraceptions.map(item => ({
-      tabClassName: 'tab', // Optional
-      panelClassName: 'panel', // Optional
-      title: item.name,
-      getContent: () => item.biography,
-    }));
-  }
-
 class Postnatal extends Component {
+  constructor(props) {
+    super(props)
+    this.getTabs = this.getTabs.bind(this)
+  }
   componentDidMount() {
     console.log(data);
     window.analytics.page();
   }
+  getTabs() {
+    return data.tab.map(item => ({
+      tabClassName: 'tab', // Optional
+      panelClassName: 'panel', // Optional
+      title: item.title,
+      getContent: () => item.text,
+    }));
+  }
   render() {
     return (
       <div className="wrapper">
-        <div dangerouslySetInnerHTML={ {__html: data.tab[0].title} } />
-        {data.tab[0].text}
-        {data.tab[0].image}
+        <Tabs items={this.getTabs()} />
       </div>
     )
   }
