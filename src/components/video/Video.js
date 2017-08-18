4e14db00de6ce2babc8ed4fc1115aeb134b9cf8a
James Liu <james_liu@fastmail.com>
2017-08-18 19:56:10 +1000

    removed unused imports

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Iframe from 'react-iframe'
 import './Video.css';
 
-  const Video = props => {
-    return (
-      <div>
-      {props.url && <div className='intrinsic-container'><Iframe
-                      url={props.url}
-                      allowFullScreen /></div>}
-      </div>
-    )
+const Video = props => {
+  return (
+    <div>
+    {props.url && <div className='intrinsic-container'><Iframe
+                    url={props.url}
+                    allowFullScreen /></div>}
+    </div>
+  )
 }
 
 export default Video
