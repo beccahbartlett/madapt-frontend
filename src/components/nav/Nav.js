712f3b534a4e4bf6caf69ffd921b1bd917e86e05
James Liu <james_liu@fastmail.com>
2017-08-18 19:13:13 +1000

    added second button to show both language options

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -10,12 +10,14 @@ class Nav extends Component {
   }
   // remove it in componendWilluNmount
   render() {
+    const lang = this.props.lang
     return (
       <div className="nav">
         <NavLink exact activeClassName='active' to='/'>
           <div className="nav-header">
             <h1 className="nav-header-title">
-              <button ref={ref => this.button = ref} onClick={this.props.toggleLanguage} className="language-btn">{this.props.lang.toUpperCase()}</button>
+              <button disabled={lang === 'en'} ref={ref => this.button = ref} onClick={this.props.toggleLanguage} className="language-btn lang-en">English</button>
+              <button disabled={lang === 'ar'} ref={ref => this.button = ref} onClick={this.props.toggleLanguage} className="language-btn lang-ar">عربى</button>
               <img className="image-banner" src={imgHeader} alt="Header Image" />
             </h1>
           </div>
