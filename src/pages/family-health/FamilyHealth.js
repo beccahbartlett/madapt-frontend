44e776f772042976b61fab1288a5dc18439e5be5
James Liu <james_liu@fastmail.com>
2017-08-18 10:49:14 +1000

    added Arabic labels for FamilyHealth

--- src/pages/family-health/FamilyHealth.js
+++ src/pages/family-health/FamilyHealth.js
@@ -15,37 +15,55 @@ class FamilyHealth extends Component {
         {
           imgSrc: WomensHealthIcon,
           theme: '5',
-          titleText: "Women's Health",
+          titleText: {
+            'en': 'Women\'s Health',
+            'ar': 'صحة المرأة'
+          },
           linkUrl: '/family-health/womens-health'
         },
         {
           imgSrc: MensHealthIcon,
           theme: '5',
-          titleText: "Men's health",
+          titleText: {
+            'en': 'Men\'s health',
+            'ar': 'صحة الرجل'
+          },
           linkUrl: '/family-health/mens-health'
         },
         {
           imgSrc: TeenHealthIcon,
           theme: '5',
-          titleText: 'Teen Health',
+          titleText: {
+            'en': 'Teen Health',
+            'ar': 'صحة الأطفال' 
+          },
           linkUrl: '/family-health/teen-health'
         },
         {
           imgSrc: ChildrensHealthIcon,
           theme: '5',
-          titleText: "Children's Health",
+          titleText: {
+            'en': 'Children\'s Health',
+            'ar': 'صحة المراهقين'
+          },
           linkUrl: '/family-health/childrens-health'
         },
         {
           imgSrc: MentalHealthIcon,
           theme: '5',
-          titleText: 'Mental Health',
+          titleText: {
+            'en': 'Mental Health',
+            'ar': 'الصحة العقلية' 
+          },
           linkUrl: '/family-health/mental-health'
         },
         {
           imgSrc: Violence,
           theme: '5',
-          titleText: 'Violence',
+          titleText: {
+            'en': 'Violence',
+            'ar': 'العنف' 
+          },
           linkUrl: '/family-health/violence'
         }
       ]
