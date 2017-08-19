dd33848ae0eb834c1da6e34ccb60d1de7ce28144
Stephen Dodd <stephen.dodd@rea-group.com>
2017-08-19 13:43:28 +1000

    feat: add hamburger menu
    -defaulted mobile menu to collapsed state
    -waiting on hamburger icon
    -will add animation as well

--- src/components/navmenu/NavMenu.js
+++ src/components/navmenu/NavMenu.js
@@ -13,7 +13,7 @@ class NavMenu extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      menu: true,
+      menu: false,
       menuItems: [
         {
           icon: iconAccess,
@@ -75,8 +75,11 @@ class NavMenu extends Component {
   render() {
     var lang = this.props.lang
     return (
-      <div>
-        <button onClick={() => this.showMenu()} className="toggleMenu">{this.state.menu ? '-' : '+'}</button>
+      <div className="menuWrapper">
+        <div className="toggleMenu">
+        <button onClick={() => this.showMenu()} className="toggleMenuButton">{`=`}</button>
+        </div>
+
         <div className="menu">
           {this.state.menuItems.map((item, index) => {
             return (
