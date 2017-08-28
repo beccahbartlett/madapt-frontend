d3b7e02cb41bb8cd29282df676e0a89d984ecab8
kit.perez <kit.perez@gmail.com>
2017-08-28 18:41:43 +1000

    - added About pages - added /about in router switch

---
+++ src/pages/about/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import AboutPageContainer from '../../components/about-page-container/AboutPageContainer'
+
+class About extends Component {
+
+  render() {
+    return (
+      <AboutPageContainer/>
+    )
+  }
+}
+
+export default About
