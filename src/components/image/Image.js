d98a9eb047d659eb8151e42a7294856c0be4ef28
James Liu <james_liu@fastmail.com>
2017-08-29 21:21:21 +1000

    finalised Image component
    -Image component receives an object containing URLs to desktop and mobile-sized images
    -CSS attached to the Image component hides the version of the image not being used based on a media query
    -ContentPageContainer has been modified to render the Image component whenever data.topLevelImage is encountered

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
+require('./Image.css')
 
-class Image extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className="image-container">
-        <img alt="" className="image-desktop" src={this.props.srcDesktop} />
-        <img alt="" className="image-mobile" src={this.props.srcMobile} />
-      </div>
-    )
-  }
+const Image = props => {
+  const srcDesktop = props.img.desktop
+  const srcMobile = props.img.mobile
+  return (
+    <div className="image-container">
+      <img alt="" className="image-desktop" src={srcDesktop} />
+      <img alt="" className="image-mobile" src={srcMobile} />
+    </div>
+  )
 }
 
 export default Image;
