269c0724371cbe30925bbaf3b62cfdc5d989691b
jamesliu <jamesliu@au1.ibm.com>
2017-09-03 14:13:04 +1000

    added footer translations and removed dead links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,31 +7,32 @@ import './Footer.css'
 class Footer extends Component {
 
   render() {
+    const lang = this.props.lang
     return (
       <div className="footer">
         <div className="footerEmergencyBanner">
-          <a href="tel:000"><IoPhone size={25} />In case of emergency call 000</a>
-          <a href="tel:131114"><IoPhone size={25} />Lifeline Phone Service 13 11 14</a>
+          <a href="tel:000"><IoPhone size={25} />{lang === 'en'? 'In case of emergency call 000' : 'في حالة الطوارئ، استدعاء 000'}</a>
+          <a href="tel:131114"><IoPhone size={25} />{lang === 'en'? 'Lifeline Phone Service 13 11 14' : 'خدمة هاتف شريان الحياة 13 11 14'}</a>
         </div>
 
         <div className="footerCore">
           <div className="footerItem">
             <ul>
               <li><Link to="/about">About mAdapt</Link></li>
-              <li><a>All Topics</a></li>
+              {/* <li><a>All Topics</a></li> */}
               <li><a href="http://www.madapt.org/contact/">Contact</a></li>
             </ul>
           </div>
 
-          <div className="footerItem footerItemBorder">
+          {/* <div className="footerItem footerItemBorder">
             <ul>
               <li><a>Statistics</a></li>
               <li><a>Premium for Clinics</a></li>
             </ul>
-          </div>
+          </div> */}
 
           <div className="footerItem social">
-            <h4>Save our website on your phone!</h4>
+            <h4>{lang === 'en' ? 'Save our website on your phone!' : 'حفظ موقعنا على الهاتف الخاص بك!'}</h4>
             <ul className="footerIcons">
               <li><SocialIcon url="https://www.facebook.com/mAdapt-1777571399195093/" network="facebook" color="white" style={{ height:23, width:23 }}/></li>
               <li><SocialIcon url="https://twitter.com/mAdapt_app" network="twitter" color="white" style={{ height:23, width:23 }}/></li>
@@ -42,7 +43,7 @@ class Footer extends Component {
 
           <div className="footerCopyright footerItem">
             <p>&copy; 2017 mAdapt. All rights reserved.</p>
-            {this.props.lang == 'en' ? <p className="disclaimer">This website and the resources to which it refers are intended to provide educational and general information only. They do not provide comprehensive medical or legal advice. Please seek specific medical or legal advice in relation to individual circumstances. mAdapt accepts no responsibility or legal liability for reliance on the information contained on this site, or other sites to which this site links.</p> : <p className="disclaimer">   ويهدف هذا الموقع والموارد التي يشير إليها إلى توفير المعلومات التعليمية والعامة فقط. وهي لا تقدم المشورة الطبية أو القانونية الشاملة. يرجى طلب المشورة الطبية أو القانونية المحددة فيما يتعلق بالظروف الفردية. mAdapt لا تتحمل أي مسؤولية أو مسؤولية قانونية عن الاعتماد على المعلومات الواردة في هذا الموقع، أو المواقع الأخرى التي يرتبط بها هذا الموقع. </p>}
+            <p className="disclaimer">{lang === 'en' ? 'This website and the resources to which it refers are intended to provide educational and general information only. They do not provide comprehensive medical or legal advice. Please seek specific medical or legal advice in relation to individual circumstances. mAdapt accepts no responsibility or legal liability for reliance on the information contained on this site, or other sites to which this site links.' : 'ويهدف هذا الموقع والموارد التي يشير إليها إلى توفير المعلومات التعليمية والعامة فقط. وهي لا تقدم المشورة الطبية أو القانونية الشاملة. يرجى طلب المشورة الطبية أو القانونية المحددة فيما يتعلق بالظروف الفردية. mAdapt لا تتحمل أي مسؤولية أو مسؤولية قانونية عن الاعتماد على المعلومات الواردة في هذا الموقع، أو المواقع الأخرى التي يرتبط بها هذا الموقع.'}</p>
           </div>
         </div>
       </div>
