6340b68a5a070c0ade4bba4145869549621ffb8a
Stephen Dodd <stephen.dodd@rea-group.com>
2017-11-25 17:21:03 +1100

    add cors for api requests

--- src/pages/healthcare-australia-cms/HealthcareAustraliaCMS.js
+++ src/pages/healthcare-australia-cms/HealthcareAustraliaCMS.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class HealthcareAustraliaCMS extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        };
+    }
 
     componentDidMount() {
-      console.log("working")
+
+      axios(
+        {
+          url: 'http://shifra.cholenasmart.com/api/healthcare',
+          method: 'get',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': "GET",
+            'Access-Control-Allow-Headers': "Content-Type, Authorization, Accept, Origin"
+        }
+       }
+      ).then(res => {
+        console.log(res)
+              //  this.setState({ data: res.data });
+        })
     }
+
     render() {
       return (
         <p>hello</p>
       )
     }
+
   }
   
   export default HealthcareAustraliaCMS
