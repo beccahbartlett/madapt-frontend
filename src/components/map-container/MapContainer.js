a2aec4eec2ba5d012a1ec546a1b4502323a87877
jamesliu <jamesliu@au1.ibm.com>
2017-11-25 14:36:09 +1100

    add google maps to MapContainer

--- src/components/map-container/MapContainer.js
+++ src/components/map-container/MapContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import './MapContainer.css'
 
 class MapContainer extends Component {
@@ -7,7 +8,12 @@ class MapContainer extends Component {
 		return (
 			<div className="sf-map-container">
 				<div className="sf-map-filters"></div>
-				<div className="sf-map-view"></div>
+				<div className="sf-map-view">
+				<GoogleMapReact
+					defaultCenter={{lat: 59.95, lng: 30.33}}
+					defaultZoom={11}
+				/>
+				</div>
 			</div>
 		)
 	}
